3dba97dcbf6225674cf228791b2967cf978be1ff
Frosty <frostyprime13@gmail.com>
2020-06-13 10:43:02 +0200

    eeee

--- src/main/java/me/mattstudios/mfgui/gui/guis/Gui.java
+++ src/main/java/me/mattstudios/mfgui/gui/guis/Gui.java
@@ -1,5 +1,6 @@
 package me.mattstudios.mfgui.gui.guis;
 
+import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.plugin.Plugin;
 
 public final class Gui extends BaseGui {
@@ -7,6 +8,8 @@ public final class Gui extends BaseGui {
     public Gui(final Plugin plugin, final int rows, final String title) {
         super(plugin, rows, title);
     }
+
+    public Gui(final Plugin plugin, final InventoryType type, final String title) { super(plugin, type, title); }
 
     public Gui(final Plugin plugin, final String title) {
         super(plugin, title);
