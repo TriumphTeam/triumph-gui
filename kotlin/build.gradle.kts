e971f35326c843c27f85c43b53446f71440b8d44
Matt <xpsykogfx@gmail.com>
2021-04-21 17:59:06 +0100

    Added Kotlin module

---
+++ kotlin/build.gradle.kts
@@ -0,0 +1,19 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.4.32"
+}
+
+dependencies {
+    implementation(project(":triumph-gui"))
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+}
+
+tasks {
+    withType<KotlinCompile> {
+        kotlinOptions {
+            jvmTarget = "1.8"
+            javaParameters = true
+        }
+    }
+}
