211311c72c7bc94ff6d49ec95347502d587d3d33
iGabyTM <contactgabytm@gmail.com>
2021-10-04 19:51:36 +0300

    add ColorableBuilder

---
+++ core/src/main/java/dev/triumphteam/gui/builder/item/ColorableBuilder.java
@@ -0,0 +1,19 @@
+package dev.triumphteam.gui.builder.item;
+
+import org.bukkit.Color;
+import org.jetbrains.annotations.NotNull;
+
+public interface ColorableBuilder<B extends BaseItemBuilder<B>> {
+
+    /**
+     * Color an {@link org.bukkit.inventory.ItemStack}
+     *
+     * @param color color
+     * @return {@link B}
+     * @see org.bukkit.inventory.meta.LeatherArmorMeta#setColor(Color)
+     * @see org.bukkit.inventory.meta.MapMeta#setColor(Color)
+     * @since 3.0.3
+     */
+    B color(@NotNull final Color color);
+
+}
