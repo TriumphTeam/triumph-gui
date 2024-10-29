c168492159116afb2e3a1faa388f3383a279d7b5
xMrAfonso <afonso.lu@hotmail.com>
2024-10-29 18:48:19 +0100

    Adds support for: - Custom Model Data (`#model()`) - Glow (`#glow()`) - Enchantments (`#enchant()` & `#disenchant()`) - Item Flags (`#flags()`) - Unbreakable (`#unbreakable()`) - Persistent Data Container (`#pdc()`)

--- paper/paper/src/main/java/dev/triumphteam/gui/paper/builder/item/AbstractItemBuilder.java
+++ paper/paper/src/main/java/dev/triumphteam/gui/paper/builder/item/AbstractItemBuilder.java
@@ -32,9 +32,13 @@ import dev.triumphteam.gui.item.items.SimpleGuiItem;
 import net.kyori.adventure.text.Component;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.persistence.PersistentDataContainer;
+import org.gradle.internal.impldep.io.usethesource.capsule.Map;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -42,13 +46,10 @@ import org.jetbrains.annotations.Nullable;
 import java.util.Arrays;
 import java.util.EnumSet;
 import java.util.List;
+import java.util.function.Consumer;
 
 @SuppressWarnings("unchecked")
 public abstract class AbstractItemBuilder<B extends AbstractItemBuilder<B>> {
-
-    private static final EnumSet<Material> LEATHER_ARMOR = EnumSet.of(
-        Material.LEATHER_HELMET, Material.LEATHER_CHESTPLATE, Material.LEATHER_LEGGINGS, Material.LEATHER_BOOTS
-    );
 
     private ItemStack itemStack;
     private ItemMeta meta;
@@ -65,7 +66,6 @@ public abstract class AbstractItemBuilder<B extends AbstractItemBuilder<B>> {
      *
      * @param name The {@link Component} name
      * @return {@link B}
-     * @since 3.0.0
      */
     @NotNull
     @Contract("_ -> this")
@@ -81,7 +81,6 @@ public abstract class AbstractItemBuilder<B extends AbstractItemBuilder<B>> {
      *
      * @param amount the amount of items
      * @return {@link B}
-     * @since 3.0.0
      */
     @NotNull
     @Contract("_ -> this")
@@ -95,7 +94,6 @@ public abstract class AbstractItemBuilder<B extends AbstractItemBuilder<B>> {
      *
      * @param lore Lore lines as varargs
      * @return {@link B}
-     * @since 3.0.0
      */
     @NotNull
     @Contract("_ -> this")
@@ -108,13 +106,192 @@ public abstract class AbstractItemBuilder<B extends AbstractItemBuilder<B>> {
      *
      * @param lore A {@link List} with the lore lines
      * @return {@link B}
-     * @since 3.0.0
      */
     @NotNull
     @Contract("_ -> this")
     public B lore(final @NotNull List<@Nullable Component> lore) {
         if (meta == null) return (B) this;
         meta.lore(lore);
+        return (B) this;
+    }
+
+    /**
+     * Set the custom data model of an item
+     *
+     * @param modelData the data of the model to be set
+     * @return {@link B}
+     */
+    @NotNull
+    @Contract("_ -> this")
+    public B model(final int modelData) {
+        if (meta == null) return (B) this;
+        meta.setCustomModelData(modelData);
+        return (B) this;
+    }
+
+    /**
+     * Makes the {@link ItemStack} glow
+     *
+     * @return {@link B}
+     */
+    @NotNull
+    @Contract(" -> this")
+    public B glow() {
+        return glow(true);
+    }
+
+    /**
+     * Adds or removes the {@link ItemStack} glow
+     *
+     * @param glow Should the item glow
+     * @return {@link B}
+     */
+    @NotNull
+    @Contract("_ -> this")
+    public B glow(boolean glow) {
+        if (glow) {
+            meta.addEnchant(Enchantment.LURE, 1, false);
+            meta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+            return (B) this;
+        }
+
+        for (final Enchantment enchantment : meta.getEnchants().keySet()) {
+            meta.removeEnchant(enchantment);
+        }
+
+        return (B) this;
+    }
+
+    /**
+     * Consumer for applying {@link PersistentDataContainer} to the item
+     *
+     * @param consumer The {@link Consumer} with the PDC
+     * @return {@link B}
+     */
+    @NotNull
+    @Contract("_ -> this")
+    public B pdc(@NotNull final Consumer<PersistentDataContainer> consumer) {
+        consumer.accept(meta.getPersistentDataContainer());
+        return (B) this;
+    }
+
+    /**
+     * Enchants the {@link ItemStack} with the specified {@link Enchantment} and level
+     *
+     * @param enchantment            The {@link Enchantment} to add
+     * @param level                  The level of the {@link Enchantment}
+     * @param ignoreLevelRestriction If should or not ignore it
+     * @return {@link B}
+     */
+    @NotNull
+    @Contract("_, _, _ -> this")
+    public B enchant(@NotNull final Enchantment enchantment, final int level, final boolean ignoreLevelRestriction) {
+        meta.addEnchant(enchantment, level, ignoreLevelRestriction);
+        return (B) this;
+    }
+
+    /**
+     * Enchants the {@link ItemStack} with the specified {@link Enchantment} and level
+     *
+     * @param enchantment The {@link Enchantment} to add
+     * @param level       The level of the {@link Enchantment}
+     * @return {@link B}
+     */
+    @NotNull
+    @Contract("_, _ -> this")
+    public B enchant(@NotNull final Enchantment enchantment, final int level) {
+        return enchant(enchantment, level, true);
+    }
+
+    /**
+     * Enchants the {@link ItemStack} with the specified {@link Enchantment}
+     *
+     * @param enchantment The {@link Enchantment} to add
+     * @return {@link B}
+     */
+    @NotNull
+    @Contract("_ -> this")
+    public B enchant(@NotNull final Enchantment enchantment) {
+        return enchant(enchantment, 1, true);
+    }
+
+    /**
+     * Enchants the {@link ItemStack} with the specified map where the value
+     * is the level of the key's enchantment
+     *
+     * @param enchantments           Enchantments to add
+     * @param ignoreLevelRestriction If level restriction should be ignored
+     * @return {@link B}
+     * @since 3.1.2
+     */
+    @NotNull
+    @Contract("_, _ -> this")
+    public B enchant(@NotNull final Map<Enchantment, Integer> enchantments, final boolean ignoreLevelRestriction) {
+        enchantments.forEach((enchantment, level) -> this.enchant(enchantment, level, ignoreLevelRestriction));
+        return (B) this;
+    }
+
+    /**
+     * Enchants the {@link ItemStack} with the specified map where the value
+     * is the level of the key's enchantment
+     *
+     * @param enchantments Enchantments to add
+     * @return {@link B}
+     * @since 3.1.2
+     */
+    @NotNull
+    @Contract("_ -> this")
+    public B enchant(@NotNull final Map<Enchantment, Integer> enchantments) {
+        return enchant(enchantments, true);
+    }
+
+    /**
+     * Disenchants a certain {@link Enchantment} from the {@link ItemStack}
+     *
+     * @param enchantment The {@link Enchantment} to remove
+     * @return {@link B}
+     */
+    @NotNull
+    @Contract("_ -> this")
+    public B disenchant(@NotNull final Enchantment enchantment) {
+        itemStack.removeEnchantment(enchantment);
+        return (B) this;
+    }
+
+    /**
+     * Add an {@link ItemFlag} to the item
+     *
+     * @param flags The {@link ItemFlag} to add
+     * @return {@link B}
+     */
+    @NotNull
+    @Contract("_ -> this")
+    public B flags(@NotNull final ItemFlag... flags) {
+        meta.addItemFlags(flags);
+        return (B) this;
+    }
+
+    /**
+     * Makes the {@link ItemStack} unbreakable
+     *
+     * @return {@link ItemBuilder}
+     */
+    @NotNull
+    @Contract(" -> this")
+    public B unbreakable() {
+        return unbreakable(true);
+    }
+
+    /**
+     * Sets the item as unbreakable
+     *
+     * @param unbreakable If should or not be unbreakable
+     * @return {@link ItemBuilder}
+     */
+    @NotNull
+    @Contract("_ -> this")
+    public B unbreakable(boolean unbreakable) {
+        meta.setUnbreakable(unbreakable);
         return (B) this;
     }
 
