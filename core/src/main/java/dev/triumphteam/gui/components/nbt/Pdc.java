83a7f65abac0254c40d059d7608ae11b6719eec9
Yive <6853318+Yive@users.noreply.github.com>
2025-01-19 14:40:50 -0800

    fix: PDC when using a library loader

--- core/src/main/java/dev/triumphteam/gui/components/nbt/Pdc.java
+++ core/src/main/java/dev/triumphteam/gui/components/nbt/Pdc.java
@@ -23,12 +23,12 @@
  */
 package dev.triumphteam.gui.components.nbt;
 
+import dev.triumphteam.gui.TriumphGui;
 import org.bukkit.NamespacedKey;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.persistence.PersistentDataType;
 import org.bukkit.plugin.Plugin;
-import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -41,7 +41,7 @@ public final class Pdc implements NbtWrapper {
     /**
      * Plugin instance required for the {@link NamespacedKey}.
      */
-    private static final Plugin PLUGIN = JavaPlugin.getProvidingPlugin(Pdc.class);
+    private static final Plugin PLUGIN = TriumphGui.getPlugin();
 
     /**
      * Sets an String NBT tag to the an {@link ItemStack}.
